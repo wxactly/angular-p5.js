69aba3ba81c4bf4435a3e99fc557cec1288910b6
Jaymz Rhime <wxactly@gmail.com>
2015-01-05 09:06:25 -0800

    Add example that demos the p5 directive

---
+++ example/example.js
@@ -0,0 +1,23 @@
+angular.module('example', [
+  'angular-p5'
+])
+.controller('ExampleCtrl', function($scope, p5) {
+  $scope.mySketch = function(sketch) {
+    var origin = new p5.Vector(100, 100);
+
+    sketch.setup = function() {
+      sketch.createCanvas(700, 410);
+    };
+
+    sketch.draw = function() {
+      var angle = sketch.radians(sketch.frameCount % 360);
+      var point = p5.Vector.fromAngle(angle)
+        .setMag(25)
+        .add(origin);
+      
+      sketch.background(0);
+      sketch.fill(255);
+      sketch.ellipse(point.x, point.y, 50, 50);
+    };
+  };
+});
